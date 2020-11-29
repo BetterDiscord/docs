ac182fb33f738ac45cca53b9b5cc577adee06c7a
Tropical <42101043+Tropix126@users.noreply.github.com>
2020-11-29 02:58:26 -0600

    Update example.plugin.js

--- examples/example.plugin.js
+++ examples/example.plugin.js
@@ -1,1 +1,16 @@
-// Todo
+/**
+ * @name Plugin Name
+ * @version 1.0.0
+ * @description A description of your plugin.
+ * @author Plugin Author
+ * @website https://betterdiscord.net/
+*/
+
+module.exports = class ExamplePlugin {
+    load() {} // Optional function. Called when the plugin is loaded in to memory
+
+    start() {} // Required function. Called when the plugin is activated (including after reloads)
+    stop() {} // Required function. Called when the plugin is deactivated
+
+    observer(changes) {} // Optional function. Observer for the document.
+}
